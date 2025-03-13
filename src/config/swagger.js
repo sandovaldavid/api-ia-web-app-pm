23a064d8b4c86535911700b6c9b1400efc995087
sandovaldavid <sandovaldavid2201@gmail.com>
2025-03-12 21:54:54 -0500

    📝 Add Swagger/OpenAPI configuration for API documentation

---
+++ src/config/swagger.js
@@ -0,0 +1,66 @@
+/**
+ * @desc    Swagger/OpenAPI configuration
+ * @author  David Sandoval
+ */
+
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('js-yaml');
+const fs = require('fs');
+const path = require('path');
+const config = require('./env');
+const { version } = require('../../package.json');
+
+// Path to OpenAPI documentation
+const openApiPath = path.join(config.rootDir, 'docs', 'api', 'openapi.yaml');
+
+// Read and parse OpenAPI definition
+const openApiDocument = YAML.load(fs.readFileSync(openApiPath, 'utf8'));
+
+// Add dynamic server info and version
+openApiDocument.info.version = version;
+openApiDocument.servers = [
+    {
+        url: `http://localhost:${config.port}/api`,
+        description: 'Local Development Server',
+    },
+    {
+        url: '/api',
+        description: 'Current Environment',
+    },
+];
+
+/**
+ * Configure Swagger middleware for Express
+ * @param {Object} app - Express app instance
+ */
+const setupSwagger = (app) => {
+    // Serve Swagger UI
+    app.use(
+        '/docs/api',
+        swaggerUi.serve,
+        swaggerUi.setup(openApiDocument, {
+            explorer: true,
+            customCss: '.swagger-ui .topbar { display: none }',
+            customSiteTitle: 'API Intermediaria - Documentación API',
+            customfavIcon: '/docs/assets/favicon.ico',
+        })
+    );
+
+    // Serve raw OpenAPI JSON
+    app.get('/docs/api.json', (req, res) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.send(openApiDocument);
+    });
+
+    // Serve raw OpenAPI YAML
+    app.get('/docs/api.yaml', (req, res) => {
+        res.setHeader('Content-Type', 'text/yaml');
+        res.send(fs.readFileSync(openApiPath, 'utf8'));
+    });
+};
+
+module.exports = {
+    setupSwagger,
+    openApiDocument,
+};
